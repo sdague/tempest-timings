3e7640081741ec2e6a86c895705c177546bf95ad
Sean Dague <sean@dague.net>
2014-01-30 15:12:32 -0500

    clean this up to be a little easier to understand

--- scrape.py
+++ scrape.py
@@ -30,6 +30,14 @@ JENKINS = (
     'jenkins07',
 )
 
+AZ = (
+    'hpcloud-az1',
+    'hpcloud-az2',
+    'hpcloud-az3',
+    'rax-iad',
+    'rax-dfw',
+    'rax-ord'
+    )
 
 def plot_data(job, fname):
     COUNT = 1
@@ -51,24 +59,21 @@ def plot_data(job, fname):
                 continue
 
             if re.search('data="4"', str(cells[0])):
-                bid = re.search('data="(\d+)"', str(cells[1]))
-                time = re.search('data="(\d+)"', str(cells[2]))
-                cloud = re.search('devstack-precise-([\w\-]+)-', str(cells[3]))
-                print "Found cloud %s => %s" % (cloud.group(1), time.group(1))
+                bid = re.search('data="(\d+)"', str(cells[1])).group(1)
+                time = re.search('data="(\d+)"', str(cells[2])).group(1)
+                cloud = re.search('devstack-precise-([\w\-]+)-', str(cells[3])).group(1)
+                print "Found cloud %s => %s" % (cloud, time)
+                if int(time) < (30 * 60 * 1000):
+                    print "Suspect run time"
+
                 data.append(dict(
-                    buildid=bid.group(1),
-                    time=time.group(1),
-                    cloud=cloud.group(1)
+                    buildid=bid,
+                    time=time,
+                    cloud=cloud
                 ))
 
-    mpdata = {
-        'hpcloud-az1': {'x': [], 'y': []},
-        'hpcloud-az2': {'x': [], 'y': []},
-        'hpcloud-az3': {'x': [], 'y': []},
-        'rax-iad': {'x': [], 'y': []},
-        'rax-dfw': {'x': [], 'y': []},
-        'rax-ord': {'x': [], 'y': []},
-    }
+    # you need very specific datastructures for matplotlib
+    mpdata = {x: {'x': [], 'y': []} for x in AZ}
 
     for d in data:
         mpdata[d['cloud']]['x'].append(COUNT)
@@ -77,24 +82,25 @@ def plot_data(job, fname):
 
     pl.figure(figsize=(8, 6))
 
-    p = pl.plot(mpdata['hpcloud-az1']['x'], mpdata['hpcloud-az1']['y'],
-                mpdata['hpcloud-az2']['x'], mpdata['hpcloud-az2']['y'],
-                mpdata['hpcloud-az3']['x'], mpdata['hpcloud-az3']['y'],
-                mpdata['rax-iad']['x'], mpdata['rax-iad']['y'],
-                mpdata['rax-dfw']['x'], mpdata['rax-dfw']['y'],
-                mpdata['rax-ord']['x'], mpdata['rax-ord']['y'],
-                )
+    axes = []
+    for az in AZ:
+        axes.append(mpdata[az]['x'])
+        axes.append(mpdata[az]['y'])
+    p = pl.plot(*axes)
+
     pl.ylim(0)
     pl.ylabel("Minutes to Complete Job")
     pl.xlabel("Run #")
     pl.title("Timing to complete job %s" % job)
-    pl.legend([p[0], p[1], p[2], p[3], p[4], p[5]],
-              ['hpcloud-az1', 'hpcloud-az2', 'hpcloud-az3',
-               'rax-iad', 'rax-dfw', 'rax-ord'],
-              loc=3
-              )
+    pl.legend(p, AZ, loc=3)
 
     pl.savefig(fname)
 
-plot_data("gate-tempest-dsvm-full", 'tempest-full.png')
-plot_data("gate-tempest-dsvm-postgres-full", 'tempest-pg-full.png')
+
+def main():
+    plot_data("gate-tempest-dsvm-full", 'tempest-full.png')
+    plot_data("gate-tempest-dsvm-postgres-full", 'tempest-pg-full.png')
+
+
+if __name__ == "__main__":
+    main()
